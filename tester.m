789a0ee1d90035dbb61144179ccbab27a3b0f941
Misspelt <lwysoglad@gmail.com>
2014-05-30 03:26:31 +0200

    clean
    - opisanie zmiennych
    - redukcja zbędnego kodu
    - podstawowe zabezpieczenia przed błędnymi danymi

--- tester.m
+++ tester.m
@@ -1,23 +1,26 @@
 clear all;
 len = 10; % Number of symbols
 M = 2; % Size of alphabet (M must be in the form of M = 2^K, where K is an integer.)
-%msg = randi([0 M-1],len,1); % Original signal
-msg=[ 0 1 ];
-[PSKmsg,PSKmsgZ,msgFi]=PSKmod(msg,M);%PSKmsgZ - zapis zespolony powinien byc taki sam jak PSKmsgML
-PSKmsgML = pskmod(msg,M);
+msg = randi([0 M-1],1,len); % Original signal
+[PSKmsg]=PSKmod(msg,M);
 
-PSKmsgNOISED = AWGNadd(PSKmsg,1);
+PSKmsgNOISED = AWGNadd(PSKmsg,100);
 
-rPSKmsgDEMOD = PSKdemod(PSKmsgNOISED,M);
+PSKmsgDEMOD = PSKdemod(PSKmsgNOISED,M);
 
-subplot(3,1,1)
+odniesienie = 1:1:len;
+subplot(2,2,1)
+plot(odniesienie,msg,'o')
+title('Plot of original message');
+
+subplot(2,2,2)
 plot(real(PSKmsg))
+title('Plot of modulated message');
 
-subplot(3,1,2)
+subplot(2,2,3)
 plot(real(PSKmsgNOISED))
+title('Plot of modulated message + noise');
 
-
-subplot(3,1,3)
-plot(rPSKmsgDEMOD)
-
-rPSKmsgDEMOD
+subplot(2,2,4)
+plot(odniesienie,PSKmsgDEMOD,'o')
+title('Plot of demodulated message');
